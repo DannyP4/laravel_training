42181a091e8d6a7f21fb95a279963f20774bd705
Long Pham <longpd1911@gmail.com>
2025-07-30 15:23:56 +0700

    9. Localization

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\LanguageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +21,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// user routes (step by step)
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
+// work in the same way but shorter
+Route::middleware('admin', 'auth')->group(function () {
+    // user routes
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/show/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::get('/users/edit/{user}', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/update/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::get('/users/delete/{user}', [UserController::class, 'showDeleteForm'])->name('users.deleteForm');
+    Route::delete('/users/destroy/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+
+    // task routes
+    Route::resource('tasks', TaskController::class);
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -27,5 +48,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('language/{lang}', [LanguageController::class, 'changeLanguage'])->name('locale');
 
 require __DIR__.'/auth.php';
